7879a2de220f06ede2b204770bbcfcca680c22aa
Tejpatel610 <tejpatel610@gmail.com>
2025-12-24 22:03:22 -0500

    feat: add profile image with tilt interaction

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -61,9 +61,23 @@ const About = () => {
           </a>
           
         </div>
-
+        {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
-          {/* Image placeholder */}
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Tej Patel"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
         </div>
       </div>
     </section>
