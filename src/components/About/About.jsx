f1382dc93f08ed91cb34b4d00343a2056c6010a5
Tejpatel610 <tejpatel610@gmail.com>
2025-12-24 22:02:02 -0500

    feat: add about text content and typing animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/logo_image.jpeg';
 
 const About = () => {
   return (
@@ -7,13 +10,56 @@ const About = () => {
       className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
     >
       <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
         <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
             Hi, I am
           </h1>
-          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4">
-            Tej Patel
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+           Tej Patel
           </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={[
+                'Fullstack Developer',
+                'Frontend Developer',
+                'App Developer',
+                'Coder',
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am a full-stack developer building scalable web applications. Skilled in both front-end and
+            back-end development, I specialize in the MERN stack and other
+            modern technologies to create seamless user experiences and
+            efficient solutions.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="https://docs.google.com/document/d/1r4oR309Omhw7APdfFR9OhTn8l5oJ7dRXOZH2GW2cg6Y/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+          
         </div>
 
         <div className="md:w-1/2 flex justify-center md:justify-end">
