26decf2dc9accd5034f83039fff255a9062fd6bf
Tejpatel610 <tejpatel610@gmail.com>
2025-12-24 22:05:24 -0500

    feat: add base skills section layout

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Skills = () => (
+  <section
+    id="skills"
+    className="py-24 px-[6vw] md:px-[8vw] lg:px-[20vw] font-sans bg-skills-gradient clip-path-custom"
+  >
+    <div className="text-center mb-12">
+      <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-white">
+        SKILLS
+      </h2>
+      <div className="w-24 h-1 bg-[#8245ec] mx-auto mt-3"></div>
+      <p className="text-gray-400 mt-4 text-sm sm:text-base lg:text-lg font-semibold max-w-3xl mx-auto">
+        A collection of my technical skills and expertise honed through various
+        projects and experiences
+      </p>
+    </div>
+  </section>
+);
+
+export default Skills;
