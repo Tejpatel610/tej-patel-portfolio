cf66b3c63216c806907b3338891469579353dbea
Tejpatel610 <tejpatel610@gmail.com>
2025-12-24 22:07:14 -0500

    feat: add base projects section layout

---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <section
+      id="work"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans relative"
+    >
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">PROJECTS</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          A showcase of the projects I have worked on, highlighting my skills
+          and experience in various technologies
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Work;
