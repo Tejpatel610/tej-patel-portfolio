7e9bc4a4d06b116fc8b800802898badb29a4b929
Tejpatel610 <tejpatel610@gmail.com>
2025-12-24 22:10:36 -0500

    feat: add education section layout and heading

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section
+      id="education"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-3"
+    >
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">EDUCATION</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          My education has been a journey of learning and development. Here are
+          the details of my academic background
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
